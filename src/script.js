7fb6f605e6d65e3f8604d6dd8f81b990cbc35cb0
maxsiosare <yoalexgg@gmail.com>
2022-01-21 13:37:12 -0800

    este

---
+++ src/script.js
@@ -0,0 +1,43 @@
+//const $contador
+const $days = document.getElementById('days'),
+$hours = document.getElementById('hours'),
+$minutes = document.getElementById('minutes'),
+$seconds = document.getElementById('seconds'),
+$finalMessage = document.querySelector('.final-sms');
+const $contador = document.querySelector('.container');
+
+//Fecha a futuro
+const countdownDate = new Date('01 17, 2022 23:08:03').getTime();
+
+let interval = setInterval(function(){
+    //Obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+    //Obtener las distancias entre ambas fechas
+    let distance = countdownDate - now;
+
+    //Calculos a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24 )) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60 )) / (1000));
+
+    //Escribimos resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    //Cuando llegue a 0
+    if(distance < 0){
+        clearInterval(interval);
+        $finalMessage.style.transform = 'translateY(5%)';
+        $contador.style.transform = 'translateY(-1000%)';
+    }
+
+    //Cuando llegue a 0
+    //if(distance < 0){
+     //   clearInterval(interval);
+     //   $contador.style.transform = 'translateY(-100)';
+   // }
+}, 1000);
